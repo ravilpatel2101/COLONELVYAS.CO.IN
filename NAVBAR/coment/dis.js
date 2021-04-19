1580489b9ab44f8fe6e0c236d998a486a944eb55
ravilpatel2101 <ravil.222012081@rfs.edu.in>
2021-04-19 22:24:20 +0530

    Lets Check

---
+++ NAVBAR/coment/dis.js
@@ -0,0 +1,10 @@
+var disqus_config = function () {
+    this.page.url = 'https://colonelvyas.co.in/NAVBAR';  
+    this.page.identifier = '36485'; 
+    };
+    (function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = 'https://ppdt-mock.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+    })();
